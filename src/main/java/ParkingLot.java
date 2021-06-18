4a488a240f242f5eb5a01c53e0ec19f3c808babf
srini <25153086+Srinivasdk@users.noreply.github.com>
2021-06-18 14:44:44 +0530

    refactoring

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -25,49 +25,35 @@ public class ParkingLot {
     }
 
     public void park(Car car) {
-        System.out.println("iam herezdvdfds");
-        System.out.println("parking lot size "+ parkingLotSize);
         if(slots.size() < parkingLotSize) {
             Slot slot = new Slot(car, false);
             String carColor = car.getColor();
-            System.out.println(" car color "+ carColor);
-            System.out.println(" car color 123 "+ carColor);
             slots.add(slot);
-            System.out.println(" iam thisere dfsdgs ertet " + slots.get(0).getCar().getRegId());
             if(!colourMap.containsKey(carColor)) {
-                System.out.println(" iam in map1");
                 List<Slot> slotList = new ArrayList<>();
                 slotList.add(slot);
                 colourMap.put(carColor, slotList);
-                System.out.println(" iam in map");
             }
             else {
-                System.out.println(" iam in map else");
                 colourMap.get(carColor).add(slot);
             }
         } else {
-            System.out.println("parking lot is full");
             return;
         }
     }
 
 
     public void leave(String regId) {
-        System.out.println("iam in removal1");
-        System.out.println("size of slots removing1 " + slots.size());
         for(Slot slot: slots) {
             if(slot.getCar().getRegId().equalsIgnoreCase(regId)) {
-                System.out.println("iam in removal");
                 slots.remove(slot);
                 colourMap.get(slot.getCar().getColor()).remove(slot);
             }
         }
-        System.out.println("size of slots removing " + slots.size());
     }
 
     public void print() {
         for(Map.Entry<String, List<Slot>> m : colourMap.entrySet()) {
-            System.out.println("color " + m.getKey() + " wrerewr");
             for(Slot slot : m.getValue()) {
                 System.out.print(slot.getCar().getRegId() + " ");
             }
--- src/main/java/ParkingLotService.java
+++ src/main/java/ParkingLotService.java
@@ -6,25 +6,21 @@ public class ParkingLotService {
     }
 
     public void createParkingLot(String size) {
-        System.out.println(" iam in creating mode");
         if(parkingLot != null) {
             System.out.println(" exception thrown since parkinglot exists");
             return ;
         }
-        System.out.println("creating parking lot");
         parkingLot = new ParkingLot(Integer.parseInt(size));
 
     }
 
     public void parkVehicle(String regId, String colour) {
         Car car  = new Car(regId, colour);
-        System.out.println(" iam  sdfdsd");
         parkingLot.park(car);
 
     }
 
     public void leaveVehicle(String regId) {
-        System.out.println("iam in removal3");
         parkingLot.leave(regId);
     }
 
